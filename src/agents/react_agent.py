801d35ed60500e75c194e70ba679b2eabd271729
Kleber Vales <klebervales.dev@gmail.com>
2026-08-12 16:42:07 -0300

    feat: creating the agent with create_agent

---
+++ src/agents/react_agent.py
@@ -0,0 +1,21 @@
+from langchain.agents import create_agent
+from langchain_openai import ChatOpenAI
+
+from src.config.settings import OPENAI_API_KEY
+from src.tools.math_tools import multiply, divide
+
+
+model = ChatOpenAI(
+    model="gpt-5.6",
+    api_key=OPENAI_API_KEY
+)
+
+tools = [
+    multiply,
+    divide
+]
+
+agent = create_agent(
+    model=model,
+    tools=tools
+)
